60e9ae3a3e4e5a47dfd3029a609a712bfa6c1355
Anshul Arora <a08786897@gmail.com>
2026-08-01 18:20:21 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (30.23%) - LeetSync

---
+++ 401-binary-watch/binary-watch.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
+        vector<string> result;
+
+        for (int h = 0; h < 12; h++) {
+            for (int m = 0; m < 60; m++) {
+                if (__builtin_popcount(h) + __builtin_popcount(m) == turnedOn) {
+                    string timeStr = to_string(h) + ":" +
+                                     (m < 10 ? "0" : "") +
+                                     to_string(m);
+                    result.push_back(timeStr);
+                }
+            }
+        }
+
+        return result;
+    }
+};
