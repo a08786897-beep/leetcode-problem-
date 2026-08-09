40fe6563e9956852d549e322e4e8736aaf372ad9
Anshul Arora <a08786897@gmail.com>
2026-08-09 12:18:43 +0530

    Time: 12 ms (61.31%) | Memory: 15.5 MB (78.49%) - LeetSync

---
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.cpp
@@ -0,0 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        vector<int> res;
+        if(root==NULL){
+            return {};
+        }
+        stack<Node*>st;
+        st.push(root);
+        while(!st.empty()){
+            Node* curr=st.top();
+            st.pop();
+            res.push_back(curr->val);
+        
+            for(Node* child: curr->children){
+                if(child != NULL){
+                    st.push(child);
+                }
+            }
+        }
+
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
