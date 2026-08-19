168a1cbc8a6bc1ca1abe7b54f74e59eb0383f72b
Anshul Arora <a08786897@gmail.com>
2026-08-19 18:40:21 +0530

    Time: 19 ms (56.78%) | Memory: 28.4 MB (78.48%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string res="";
+        for(char ch:s){
+            if(ch == '*'){
+                if(!res.empty()){
+                    res.pop_back();
+                }
+            }
+
+            else{
+                res.push_back(ch);
+            }
+        }
+        return res;
+    }
+};
