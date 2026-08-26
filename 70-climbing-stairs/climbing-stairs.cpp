525e055046a345e104cb75d19eadc37747632c67
Anshul Arora <a08786897@gmail.com>
2026-08-27 01:23:59 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (98.90%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -3,15 +3,17 @@ public:
     int climbStairs(int n) {
         if(n==1||n==2) return n;
 
-        vector<int> dp(n+1);
+        int prev2=1;
+        int prev1=2;
 
-        dp[1]=1;
-        dp[2]=2;
+        int result=prev1;
 
         for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            result=prev1+prev2;
+            prev2=prev1;
+            prev1=result;
         }
 
-        return dp[n];
+        return result;
     }
 };
