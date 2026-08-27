1371df5f0e77fcc8b40bd5a65a76dc1a0f898236
Anshul Arora <a08786897@gmail.com>
2026-08-27 23:16:06 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (24.12%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -5,17 +5,15 @@ public:
 
         if(n==1) return nums[0];
 
-        
-        int prev1=nums[0];
-        int prev2=max(nums[0],nums[1]);
-        int res=prev2;
+        vector<int> dp(n);
+
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
 
 
         for(int i=2;i<n;i++){
-            res=max(prev2,prev1+nums[i]);
-            prev1=prev2;
-            prev2=res;
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
         }
-        return res;
+        return dp[n-1];
     }
 };
