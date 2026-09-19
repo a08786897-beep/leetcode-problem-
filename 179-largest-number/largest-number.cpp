32fbb3b39756b37035e0bbd8b3ff278f2bf520bc
Anshul Arora <a08786897@gmail.com>
2026-09-19 18:52:24 +0530

    Time: 4 ms (50.90%) | Memory: 17.2 MB (33.26%) - LeetSync

---
+++ 179-largest-number/largest-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        vector<string> st;
+
+        for(int num : nums){
+            st.push_back(to_string(num));
+        }
+
+        sort(st.begin(),st.end(),[](string &a,string &b){
+            return a+b>b+a;
+        });
+
+        if(st[0]=="0"){
+            return "0";
+        }
+
+        string res="";
+        for(string& str: st){
+            res+=str;
+        }
+        return res;
+    }
+};
